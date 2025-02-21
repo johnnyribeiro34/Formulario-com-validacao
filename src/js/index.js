4a0df666d48f4350e645781bbdec9d728212a91f
Johnny Ribeiro <johnnyribeirobezerra@gmail.com>
2025-02-20 22:01:15 -0300

    melhorado identação do código do js

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,29 @@
+document
+  .getElementById("meuFormulario")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Impede o envio do formulário se houver erros
 
+    const inputs = document.querySelectorAll("#campo");
+    let formValido = true;
 
-document.getElementById('meuFormulario').addEventListener('submit', function(event) {
-  event.preventDefault(); // Impede o envio do formulário se houver erros
-
-    const inputs = document.querySelectorAll('#campo');
-    let formValido= true;
-
-    inputs.forEach(campo => {
-      campo.classList.remove('error');
-      campo.nextElementSibling.textContent = '';
-    
-  });
-
-     // Valida cada campo
-    inputs.forEach(campo => {
-        if (campo.value.trim() === '') {
-            campo.classList.add('error'); // Adiciona a classe de erro (borda vermelha)
-            campo.nextElementSibling.textContent = 'Campo obrigatório'; // Exibe mensagem de erro
-            formValido = false;
-        } else if (campo.value !==''){
-          campo.classList.add('correto')
-
-        }     
-        
+    inputs.forEach((campo) => {
+      campo.classList.remove("error");
+      campo.nextElementSibling.textContent = "";
     });
-   
+
+    // Valida cada campo
+    inputs.forEach((campo) => {
+      if (campo.value.trim() === "") {
+        campo.classList.add("error"); // Adiciona a classe de erro (borda vermelha)
+        campo.nextElementSibling.textContent = "Campo obrigatório"; // Exibe mensagem de erro
+        formValido = false;
+      } else if (campo.value !== "") {
+        campo.classList.add("correto");
+      }
+    });
+
     if (formValido) {
-      alert('Formulário enviado com sucesso!');
+      alert("Formulário enviado com sucesso!");
       this.submit();
     }
-
-   });
-
-
+  });
